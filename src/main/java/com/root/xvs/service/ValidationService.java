44811ad4ce0753f89b0c1adbc6d599ade9df5123
sk1@assaycr.com <satish.achary1991@gmail.com>
2020-09-25 16:06:53 +0530

    initial

---
+++ src/main/java/com/root/xvs/service/ValidationService.java
@@ -0,0 +1,6 @@
+package com.root.xvs.service;
+
+public interface ValidationService {
+    void validate();
+    void generateXml();
+}
---
+++ src/main/java/com/root/xvs/serviceimpl/ValidationServiceImpl.java
@@ -0,0 +1,20 @@
+package com.root.xvs.serviceimpl;
+
+import com.root.xvs.service.ValidationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class ValidationServiceImpl implements ValidationService {
+
+    @Override
+    public void validate() {
+
+    }
+
+    @Override
+    public void generateXml() {
+
+    }
+}
