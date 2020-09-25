37d3d46e13cc327cc949e45665e8ae20503035cf
sk1@assaycr.com <satish.achary1991@gmail.com>
2020-09-25 15:43:03 +0530

    initial commit

---
+++ src/main/java/com/root/xvs/XmlValidatorApplication.java
@@ -0,0 +1,11 @@
+package com.root.xvs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlValidatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XmlValidatorApplication.class, args);
+    }
+}
